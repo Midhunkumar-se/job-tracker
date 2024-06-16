d5b5966b734745bad5f08a649b9f2a64857f7f9a
Midhunkumar-se <midhu025@gmail.com>
2024-06-16 14:06:08 +0530

    update user API route and complete profile page functionality

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { logout, test } from "../controllers/user.controller.js";
+import { test, updateUser, logout } from "../controllers/user.controller.js";
+import { verifyToken } from "../middleware/verifyUser.js";
 
 const router = express.Router();
 
 router.get("/test", test);
 router.post("/logout", logout);
+router.put("/update/:userId", verifyToken, updateUser);
 
 export default router;
--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -28,10 +28,30 @@ const userSlice = createSlice({
       state.error = null;
       state.loading = false;
     },
+    updateStart: (state) => {
+      state.loading = true;
+      state.error = null;
+    },
+    updateSuccess: (state, action) => {
+      state.currentUser = action.payload;
+      state.loading = false;
+      state.error = null;
+    },
+    updateFailure: (state, action) => {
+      state.error = action.payload;
+      state.loading = false;
+    },
   },
 });
 
-export const { loginStart, loginSuccess, loginFailure, logoutSuccess } =
-  userSlice.actions;
+export const {
+  loginStart,
+  loginSuccess,
+  loginFailure,
+  logoutSuccess,
+  updateStart,
+  updateSuccess,
+  updateFailure,
+} = userSlice.actions;
 
 export default userSlice.reducer;
