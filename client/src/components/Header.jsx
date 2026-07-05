746bc22fbed06d8356b495db94206f2b5441d08c
Midhunkumar-se <midhu025@gmail.com>
2026-07-05 23:12:42 +0530

    bg color

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -28,7 +28,7 @@ function Header() {
   };
 
   return (
-    <Navbar className="shadow-sm">
+    <Navbar className="shadow-sm bg-inherit">
       <Link to="/">
         <div className="max-w-[1200px] m-auto p-2.5 pl-5">
           <img src={logo} className="w-[170px] h-[30px]" />
