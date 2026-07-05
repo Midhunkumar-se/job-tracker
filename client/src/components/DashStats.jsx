f840ed96128a339042738d34472abd54a314fefb
Midhunkumar-se <midhu025@gmail.com>
2026-07-05 22:45:03 +0530

    small change

--- client/src/components/DashStats.jsx
+++ client/src/components/DashStats.jsx
@@ -1,4 +1,6 @@
 import { FaSuitcaseRolling, FaCalendarCheck, FaBug } from "react-icons/fa";
+import { GiChecklist, GiSellCard } from "react-icons/gi";
+
 import React, { useEffect, useState } from "react";
 import AreaChartComponent from "./AreaChart";
 import BarChartComponent from "./BarChart";
@@ -30,40 +32,43 @@ const DashStats = () => {
         <div className="flex flex-col p-8 pt-2 box-shadow-2 rounded-md border-b-4 border-b-yellow-800">
           <div className="flex justify-between items-center pt-6">
             <h1 className="text-yellow-800 text-4xl font-medium">
-              {stat.pending}
+              {8}
             </h1>
             <div className="p-3 rounded-sm pending text-4xl">
-              <FaSuitcaseRolling />
+              {/* <FaSuitcaseRolling />
+               */}
+              <GiSellCard />
             </div>
           </div>
-          <p className="pt-6">Pending Applications</p>
+          <p className="pt-6">Sold Properties</p>
         </div>
         <div className="flex flex-col p-8 pt-2 box-shadow-2 rounded-md border-b-4 border-b-purple-900">
           <div className="flex justify-between items-center pt-6">
             <h1 className="text-purple-900 text-4xl font-medium">
-              {stat.interview}
+              {25}
             </h1>
             <div className="p-3 rounded-sm interview text-4xl">
-              <FaCalendarCheck />
+              <GiChecklist />
             </div>
           </div>
-          <p className="pt-6">Interviews Sheduled</p>
+          <p className="pt-6">Listed Properties</p>
         </div>
         <div className="flex flex-col p-8 pt-2 box-shadow-2 rounded-md border-b-4 border-b-red-800">
           <div className="flex justify-between items-center pt-6">
             <h1 className="text-red-800 text-4xl font-medium">
-              {stat.declined}
+              {10}
             </h1>
             <div className="p-3 rounded-sm declined text-4xl">
               <FaSuitcaseRolling />
+             
             </div>
           </div>
-          <p className="pt-6">Jobs Declined</p>
+          <p className="pt-6">Rented Properties</p>
         </div>
       </div>
       <div className="mt-10">
         <div className="flex flex-col items-center">
-          <h1 className="text-xl font-medium mb-5">Monthly Applications</h1>
+          <h1 className="text-xl font-medium mb-5">Monthly Trends</h1>
           <button
             className="purple-button"
             type="button"
